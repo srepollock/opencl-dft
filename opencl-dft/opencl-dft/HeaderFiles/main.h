4985a01ff526f9eac5ad1416e896a71ebd11719a
Spencer Pollock <srepollock@gmail.com>
2017-10-18 20:31:22 -0700

    Added OpenCL headers

---
+++ opencl-dft/opencl-dft/HeaderFiles/main.h
@@ -0,0 +1,5 @@
+#pragma once
+#include <iostream>
+#include <thread>
+#include <chrono>
+#include "CL\opencl.h"
--- opencl-dft/opencl-dft/SourceFiles/main.cpp
+++ opencl-dft/opencl-dft/SourceFiles/main.cpp
@@ -2,4 +2,5 @@
 int main(int argc, char ** argv)
 {
 	std::cout << "Hello World" << std::endl;
+	std::this_thread::sleep_for(std::chrono::seconds(10));
 }
