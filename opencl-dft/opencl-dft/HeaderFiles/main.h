d884a97f1c3eebc6cdddd9ba0af8c7844c671191
Spencer Pollock <spencer@spollock.ca>
2017-10-18 20:40:25 -0700

    :octocat: Silly fixes

--- opencl-dft/opencl-dft/HeaderFiles/main.h
+++ opencl-dft/opencl-dft/HeaderFiles/main.h
@@ -1,5 +1,9 @@
 #pragma once
+#ifdef __APPLE__
+#include "CL/opencl.h"
+#elif defined _WIN32 || defined _WIN64
+#include "CL\opencl.h"
+#endif
 #include <iostream>
 #include <thread>
 #include <chrono>
-#include "CL\opencl.h"
